11effce630477ae966bcd44bb15d3076e09e6cca
MateoGuevara09 <97854957+MateoGuevara09@users.noreply.github.com>
2023-06-14 09:22:30 -0300

    mas o menos funciona

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Obtenerbandera from './components/obtenerbandera';
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('');
-  fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
+  const [country, setCountry] = useState([]);
+  useEffect(() => {
+    fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
     .then(res => res.json())
     .then(res => {
-      //da un error que logea todo el tiempo(No se porque)
       setCountries(res.data);
+      const x = Math.floor(Math.random()*220)
+      setCountry(res.data[x])
     })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <h1>React App</h1>
--- src/components/obtenerbandera.jsx
+++ src/components/obtenerbandera.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 const ObtenerBandera = (props) => {
-    function obtenerBandera() {
-        //crea numeri aleatorio entre 0 y 220 para obtener una bandera aleatoria
-        //setea a country por el pais y muestra en el img la imagen de la bandera
-        var x = Math.random()*220;
-        props.setCountry(props.countries.data[x])
-    }
+
+    const newFlag = () => {
+        
+    }    
+    
     return (
         <div>
-        <img src={props.country.flag} alt="Bandera" />
+        {props.country && <img src={props.country.flag} alt="Bandera" /> }
         </div>
     );
 }
